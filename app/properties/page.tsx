3144c5987b5a762149e44a45130c58c23bea2101
genekellyboyle <DrDuffy@bhhsnv.com>
2026-01-02 23:44:04 -0800

    Fix import statement for RealScoutAdvancedSearch component

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import RealScoutListings from '@/components/RealScoutListings';
+import RealScoutAdvancedSearch from '@/components/RealScoutAdvancedSearch';
 import { generateBreadcrumbSchema } from '@/lib/breadcrumbs';
 
 export const metadata: Metadata = {
